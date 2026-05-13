abc7eea3f8bce514cc91f996195f7e468a37a32a
Nguyen Dang Khang Huy <vietnamnhungchuyendi@gmail.com>
2026-05-13 17:04:09 +0700

    hoan thanh bai demo tinh tong

---
+++ Slot2/demo/app.js
@@ -0,0 +1,9 @@
+// Bài toán tính tổng đơn giản bằng Node.js
+const so1 = 10;
+const so2 = 20;
+const tong = so1 + so2;
+
+console.log("--- Demo Node.js ---");
+console.log(`Số thứ nhất: ${so1}`);
+console.log(`Số thứ hai: ${so2}`);
+console.log(`=> Tổng là: ${tong}`);
